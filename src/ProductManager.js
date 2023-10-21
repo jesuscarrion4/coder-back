a77abf89e5031c825fa4ac1facb70457ac182fcd
jesus carrion <jesusdcarriong@gmail.com>
2023-10-21 11:14:43 -0300

    mejoras desafio

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,15 +1,41 @@
+import fs from 'fs'
+
 class ProductManager {
-  constructor() {
-    this.products = [];
+  constructor(path) {
+    this.path = path;
+    this.products = this.leerProductos();
     this.nextId = 1;
   }
 
-  getProducts() {
-    return this.products;
+  leerProductos() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf-8');
+      if (data === '') {
+        return [];
+      }
+      return JSON.parse(data);
+    } catch (err) {
+      console.error('Error al leer los productos:', err);
+      return [];
+    }
+  }
+
+  guardarProductos() {
+    try {
+      const data = JSON.stringify(this.products, null, 2);
+      fs.writeFileSync(this.path, data, 'utf-8');
+      console.log('Productos guardados correctamente');
+    } catch (err) {
+      console.error('Error al guardar los productos:', err);
+    }
   }
 
   addProduct(productData) {
     const { title, description, price, thumbnail, code, stock } = productData;
+
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error('Todos los campos son obligatorios.');
+    }
 
     if (this.products.some(product => product.code === code)) {
       throw new Error('El código del producto ya está en uso.');
@@ -27,89 +53,71 @@ class ProductManager {
 
     this.products.push(newProduct);
     this.nextId++;
+    this.guardarProductos();
 
     return newProduct;
   }
 
   getProductById(id) {
-    const product = this.products.find(product => product.id === id);
-    if (!product) {
-      throw new Error('Producto no encontrado.');
-    }
-    return product;
+    return this.products.find(product => product.id === id);
   }
 
-  updateProduct(id, updatedData) {
+  updateProduct(id, updatedProduct) {
     const productIndex = this.products.findIndex(product => product.id === id);
+
     if (productIndex === -1) {
-      throw new Error('Producto no encontrado.');
+      return;
     }
 
-    this.products[productIndex] = { ...this.products[productIndex], ...updatedData };
+    this.products[productIndex] = {
+      id,
+      ...updatedProduct,
+    };
 
-    return this.products[productIndex];
+    this.guardarProductos();
+    console.log('Producto actualizado');
   }
 
   deleteProduct(id) {
     const productIndex = this.products.findIndex(product => product.id === id);
+
     if (productIndex === -1) {
-      throw new Error('Producto no encontrado.');
+      return console.log('Producto no encontrado');
     }
 
     this.products.splice(productIndex, 1);
+    this.guardarProductos();
+    console.log('Producto eliminado');
+  }
+
+  getProducts() {
+    return this.products;
   }
 }
 
+const manager = new ProductManager('./data.json');
 
-const productManager = new ProductManager();
-
-
-const products = productManager.getProducts();
-console.log(products); // []
-
-
-const newProduct = productManager.addProduct({
-  title: 'caffe americano',
-  description: 'granos finos',
-  price: 200,
-  thumbnail: 'imagen',
-  code: 'abc123',
-  stock: 25,
+manager.addProduct({
+  title: "cafe americano",
+  description: "granos",
+  price: 2.99,
+  thumbnail: "img/cafe.jpg",
+  code: "5679",
+  stock: 10,
 });
-console.log(newProduct);
 
+console.log(manager.getProducts());
 
-const updatedProducts = productManager.getProducts();
-console.log(updatedProducts);
+const product = manager.getProductById(1);
+console.log(product);
 
+manager.updateProduct(1, {
+  title: "moka molido",
+  description: "ganos",
+  price: 3.99,
+  thumbnail: "img/moka.jpg",
+  code: "5679",
+  stock: 5,
+});
 
-try {
-  const productById = productManager.getProductById(newProduct.id);
-  console.log(productById);
-} catch (error) {
-  console.error(error.message);
-}
-
-
-try {
-  const updatedProduct = productManager.updateProduct(newProduct.id, { price: 250 });
-  console.log(updatedProduct);
-} catch (error) {
-  console.error(error.message);
-}
-
-
-try {
-  productManager.deleteProduct(newProduct.id);
-  console.log('Producto eliminado');
-} catch (error) {
-  console.error(error.message);
-}
-
-
-try {
-  productManager.deleteProduct(newProduct.id);
-  console.log('Producto eliminado');
-} catch (error) {
-  console.error(error.message);
-}
+manager.deleteProduct(2);
